ec02b7f1554c822c197b58e6dcfa976b475eef29
jorge <jorge@CL-IT02528.>
2025-02-15 23:50:50 -0300

    updated tar nvim

--- install.sh
+++ install.sh
@@ -118,7 +118,7 @@ install_nvim() {
         *)
             curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
             sudo rm -rf /opt/nvim
-            sudo tar -C /opt -xzf nvim-linux64.tar.gz
+            sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
             echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >>~/.zshrc
             ;;
         esac
