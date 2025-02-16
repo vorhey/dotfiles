2e818553fa5cfe74334ee8fb844b03ba75eeb19f
jorge <jorge@CL-IT02528.>
2025-02-15 23:55:22 -0300

    updated nvim path

--- install.sh
+++ install.sh
@@ -119,7 +119,7 @@ install_nvim() {
             curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
             sudo rm -rf /opt/nvim
             sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-            echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >>~/.zshrc
+            echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >>~/.zshrc
             ;;
         esac
         echo "nvim installed"
