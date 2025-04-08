73867ce0302896c762cd573094efe983edd8ad36
vorhey <jorgeherreraulloa@gmail.com>
2025-04-08 15:56:43 -0400

    updated distro icons

--- get_distro_icon.sh
+++ get_distro_icon.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 case "$(cat /etc/os-release | grep '^ID=' | cut -d'=' -f2 | tr -d '"')" in
-"ubuntu") echo "#[fg=red]󰕈" ;;
-"debian") echo "#[fg=red]" ;;
-"fedora") echo "#[fg=blue]" ;;
-"arch") echo "#[fg=blue]󰣇" ;;
-"cachyos") echo "#[fg=blue]󰣇" ;;
-"opensuse-tumbleweed") echo "#[fg=green]" ;;
+"ubuntu") echo "#[fg=red]󰕈 " ;;
+"debian") echo "#[fg=red] " ;;
+"fedora") echo "#[fg=blue] " ;;
+"arch") echo "#[fg=blue]󰣇 " ;;
+"cachyos") echo "#[fg=blue]󰣇 " ;;
+"pop") echo "#[fg=orange] " ;;
+"opensuse-tumbleweed") echo "#[fg=green] " ;;
 *) echo "" ;;
 esac
